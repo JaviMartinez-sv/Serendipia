ae0119620eed8a461cb1b34de677e98425acbc88
JRow <javijog@gmail.com>
2021-05-27 10:38:16 +0200

    Añado productos para front

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div class="hello">
    <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Serendipia</a>
+    <router-link class="navbar-brand">Serendipia</router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -10,6 +10,9 @@
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <router-link to="/home" class="nav-link active" aria-current="page" href="#">Home</router-link>
+        </li>
+        <li class="nav-item">
+              <router-link to="/products" class="nav-link" href="#">Products</router-link>
         </li>
         <li class="nav-item">
           <router-link to="/about" class="nav-link" href="#">About</router-link>
