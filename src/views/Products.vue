bf1467d45b6daa0790e3e95b3fd628f1f87c00ed
JRow <javijog@gmail.com>
2021-06-05 15:33:36 +0200

    Fallo en v-foe

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -36,53 +36,130 @@
 
           <h3>Products list</h3>
           
-          <table>
+          <div class="table-responsive"></div>
+        
+          <table class="table">
           <thead>
             <tr>
               <th>Name</th>
               <th>Price</th>
+              <th>Modify</th>
             </tr>
           </thead>
 
           <tbody>
-                <tr v-for-key="product in products">
-                <td> {{product.name}} </td>
-                <td> {{product.price}} </td>
+
+                <tr v-for="product in products" v-bind:key="product">
+                <td> {{product.data().name}} </td>
+                <td> {{product.data().price}} </td>
+                <td>
+                  <button @click="editProduct(product)" class="btn btn-primary">Edit</button>
+                  <button @click="deleteProduct(product.id)" class="btn btn-danger">Delete</button>
+                </td>
                 </tr> 
+
+                
+
+
+
+
+
 
           </tbody>
           </table>
-        
          </div>
+      </div>
+
+ <!-- Modal -->
+      <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="editLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="editLabel">Edit Product</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+
+                <div class="form-group">
+                  <input type="text" placeholder="Product Name" v-model="product.name" class="form-control">
+               </div>
+
+             <div class="form-group">
+                <input type="text" placeholder="Price" v-model="product.price" class="form-control">
+              </div>
+
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button @click="updateProduct()" type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
       </div>
   </div>
 </template>
 
 <script>
 import {db} from '../firebase';
+import $ from "jquery";
 
 export default {
   name: "Products",
   props: {
     msg: String
   },
+
   data(){
     return{
       products:[],
       product:{
-      name:null,
-      price:null    
-      }
+        name:null,
+        price:null    
+      },
+      activeItem:null
       
     }
   },
   methods:{
+    updateProduct(){
+
+      db.collection("products").doc("DC").update({
+        capital:true
+      })
+      .then(function(){
+        console.log("Document successfully updated!");
+      })
+      .catch(function(error){
+        console.error("Error updating document: ", error);
+      });
+
+    },
+
+    editProduct(product){
+      $('#edit').modal('show');
+      this.product = product.data();
+      this.activeItem=product.id;
+    },
+    deleteProduct(doc){
+      if(confirm('Are you sure?')){
+        db.collection("products").doc(doc).delete().then(function(){
+          console.log("Document succesfully deleted!");
+        }).catch(function(error){
+          console.error("Error removing document: ", error);
+        });
+      }else{
+          alert(doc);
+      }
+
+    },
     readData(){
-      db.collection('products').get().then((querySnapshot) =>{
+      db.collection("products").get().then((querySnapshot) =>{
 
         querySnapshot.forEach((doc) => {
 
-           this.products.push(doc.data());
+           this.products.push(doc);
         });
       });
 
